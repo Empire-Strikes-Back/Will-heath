f51eda36acfeaa1de64c906e0c92af3ad5469f9b
sportleafs <sportleafs@program>
2021-08-01 04:29:48 +0300

    from program.sh i remove no-history push

--- program.sh
+++ program.sh
@@ -5,15 +5,4 @@ main(){
   # node src/sportleafs/main.js
 }
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am sportleafs program"
-  git push -f -u origin main
-}
-
 "$@"
