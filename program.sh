a23d705a88d43d6714df0989c28871f42f5d5367
sportleafs <sportleafs@program>
2021-08-03 15:52:24 +0300

    i use deps-repl library

--- program.sh
+++ program.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+repl(){
+  clj \
+    -X:repl deps-repl.core/process \
+    :main-ns sportleafs.main \
+    :port 7788 \
+    :host '"0.0.0.0"' \
+    :repl? true \
+    :nrepl? false
+}
+
 main(){
   node src/sportleafs/cluster.js
   # node src/sportleafs/main.js
