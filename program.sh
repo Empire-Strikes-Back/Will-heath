76a7315ecc31d9dcc4a0a77df8da6dcc37a218a0
sportleafs <sportleafs@program>
2021-08-03 20:59:40 +0300

    i revert "to program.sh i add j-package"
    This reverts commit ce2eaed500814fae30a1ce2c9d2a53f5dbc156a2.

--- program.sh
+++ program.sh
@@ -26,30 +26,4 @@ uberjar(){
   mv out/sportleafs.standalone.jar out/jpackage-input/
 }
 
-j-package(){
-  OS=${1:?"Need OS type (windows/linux/mac)"}
-
-  echo "Starting compilation..."
-
-  if [ "$OS" == "windows" ]; then
-    J_ARG="--win-menu --win-dir-chooser --win-shortcut"
-          
-  elif [ "$OS" == "linux" ]; then
-      J_ARG="--linux-shortcut"
-  else
-      J_ARG=""
-  fi
-
-  jpackage \
-    --input out/jpackage-input \
-    --dest out \
-    --main-jar sportleafs.standalone.jar \
-    --name "sportleafs" \
-    --main-class clojure.main \
-    --arguments -m \
-    --arguments sportleafs.main \
-    --app-version "1" \
-    $J_ARG
-}
-
 "$@"
