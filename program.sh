9eaf5a0b6ffb1abcd4edece845c851a87f01cf64
sportleafs <sportleafs@program>
2021-08-03 15:53:32 +0300

    for creating uberjar i use genie library

--- program.sh
+++ program.sh
@@ -17,4 +17,13 @@ main(){
     -M -m sportleafs.main
 }
 
+uberjar(){
+  clj \
+    -X:uberjar genie.core/process \
+    :uberjar-name out/sportleafs.standalone.jar \
+    :main-ns sportleafs.main
+  mkdir -p out/jpackage-input
+  mv out/sportleafs.standalone.jar out/jpackage-input/
+}
+
 "$@"
